32288295d89df8ed93ad02a0aba22ca7e3172eb2
21dbly <21dbly@gmail.com>
2025-10-26 01:22:10 -0600

    added placeholder sprite for corn and made now plant_manager plants it

--- super-simple-spooky-seed-simulator/Scripts/plant_manager.gd
+++ super-simple-spooky-seed-simulator/Scripts/plant_manager.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
 @export var tile_map: TileMapLayer
+var corn_plant_scene = preload("res://Scenes/corn_plant.tscn")
 
 func plant(player_coords: Vector2) -> void:
 	var tile_coords = tile_map.local_to_map(self.to_local(player_coords))
 	var tile_data = tile_map.get_cell_tile_data(tile_coords)
 	if tile_data.get_custom_data("plantable"):
-		print("plant")
+#		TODO: check if there is already a plant there
+		var corn_plant = corn_plant_scene.instantiate()
+		add_child(corn_plant)
+		corn_plant.position = tile_map.map_to_local(tile_coords)
 		
 func _input(event):
 #	planting with mouse click for testing
