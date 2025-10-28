bee82403dc32adbf19f020f88c3fc34c6c9ee8e3
21dbly <21dbly@gmail.com>
2025-10-28 00:12:36 -0600

    plants can be harvested

--- super-simple-spooky-seed-simulator/Scripts/corn_plant.gd
+++ super-simple-spooky-seed-simulator/Scripts/corn_plant.gd
@@ -1,1 +1,9 @@
+class_name CornPlant
 extends Node2D
+
+func _ready() -> void:
+	print("planted")
+
+func harvest():
+	print("harvested")
+	queue_free()
--- super-simple-spooky-seed-simulator/Scripts/plant_manager.gd
+++ super-simple-spooky-seed-simulator/Scripts/plant_manager.gd
@@ -2,17 +2,21 @@ extends Node2D
 
 @export var tile_map: TileMapLayer
 var corn_plant_scene = preload("res://Scenes/corn_plant.tscn")
+var plants: Dictionary[Vector2i, CornPlant] = {}
 
-func plant(player_coords: Vector2) -> void:
+func plant_or_harvest(player_coords: Vector2i) -> void:
 	var tile_coords = tile_map.local_to_map(self.to_local(player_coords))
 	var tile_data = tile_map.get_cell_tile_data(tile_coords)
 	if tile_data.get_custom_data("plantable"):
-#		TODO: check if there is already a plant there
-		var corn_plant = corn_plant_scene.instantiate()
-		add_child(corn_plant)
-		corn_plant.position = tile_map.map_to_local(tile_coords)
+		if tile_coords not in plants or plants[tile_coords] == null:
+			var corn_plant = corn_plant_scene.instantiate()
+			add_child(corn_plant)
+			corn_plant.position = tile_map.map_to_local(tile_coords)
+			plants[tile_coords] = corn_plant
+		else:
+			plants[tile_coords].harvest()
 		
 func _input(event):
 #	planting with mouse click for testing
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		plant(get_global_mouse_position())
+		plant_or_harvest(get_global_mouse_position())
