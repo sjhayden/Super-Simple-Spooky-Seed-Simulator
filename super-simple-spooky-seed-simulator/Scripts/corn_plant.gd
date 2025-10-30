04cca842c182cb935938133c263e24071558410b
21dbly <21dbly@gmail.com>
2025-10-30 03:50:54 -0600

    jacks can eat plants

--- super-simple-spooky-seed-simulator/Scripts/corn_plant.gd
+++ super-simple-spooky-seed-simulator/Scripts/corn_plant.gd
@@ -4,24 +4,38 @@ extends Node2D
 @onready var anim := $AnimatedSprite2D
 @onready var grow_timer := $GrowTimer
 var growth_level = 0
-const anim_strings = ["sprout", "growing", "full_grown"]
+const anim_growing_strings = ["sprout", "growing", "full_grown"]
+const anim_eating_string = ["eaten_3", "eaten_2", "eaten_1"]
 const MAX_GROWTH_LEVEL = 2
 var num_corns = 3
 
 func _ready() -> void:
-	anim.play(anim_strings[growth_level]) # unnecessary?
+	anim.play(anim_growing_strings[growth_level]) # unnecessary?
 
 func harvest():
-	if growth_level == MAX_GROWTH_LEVEL:
+	if growth_level == MAX_GROWTH_LEVEL or num_corns == 0:
 		GameManager.change_num_corns(num_corns)
 		queue_free()
+		
+func bite():
+	grow_timer.stop()
+	if growth_level == 0:
+		anim.play("eaten_sprout")
+		num_corns = 0
+	elif growth_level == 1:
+		anim.play("eaten_growing")
+		num_corns = 0
+	elif growth_level == MAX_GROWTH_LEVEL:
+		if num_corns > 0: num_corns -= 1
+		anim.play(anim_eating_string[num_corns])
 
 
 func _on_grow_timer_timeout() -> void:
 	if growth_level < MAX_GROWTH_LEVEL:
 		growth_level += 1
-	anim.play(anim_strings[growth_level])
+	anim.play(anim_growing_strings[growth_level])
 	if growth_level >= MAX_GROWTH_LEVEL:
 		grow_timer.stop()
+	
 		
 	
--- super-simple-spooky-seed-simulator/Scripts/jack_o_lantern.gd
+++ super-simple-spooky-seed-simulator/Scripts/jack_o_lantern.gd
@@ -2,10 +2,13 @@ extends CharacterBody2D
 
 @onready var anim = $AnimatedSprite2D
 @onready var hesitation = $HesitationTimer
+@onready var bite_timer = $BiteTimer
 var hits_taken := 0
 var target # Vector2 or null
 var SPEED := 100
 var SIGHT_RADIUS := 1000
+var BITE_RANGE := 20
+var eaten_plants_locations = {} # keeps track of plants this jack has already found dead so they don't just stay their eating a dead plant
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
 	if area == Global.playerDamageZone:
@@ -24,33 +27,67 @@ func _ready() -> void:
 func _physics_process(_delta: float) -> void:
 	if target == null:
 		velocity = Vector2(0, 0)
+	elif global_position.distance_to(target) <= BITE_RANGE:
+		try_to_bite()
 	else:
 		var direction = global_position.angle_to_point(target)
 		velocity = Vector2(SPEED, 0.0).rotated(direction)
 		
 	move_and_slide()
 	
-func find_nearest_plant_position():
+func find_nearest_plant():
 	var plants = get_tree().get_nodes_in_group("Plants")
 	var closest_distance := SIGHT_RADIUS
 	var closest_plant: CornPlant
 	var distance
 	for plant:CornPlant in plants:
+		if plant.global_position in eaten_plants_locations: continue
 		distance = global_position.distance_to(plant.global_position)
 		if distance < closest_distance:
 			closest_distance = distance
 			closest_plant = plant
-	if closest_plant == null:
+	return closest_plant
+	
+func find_nearest_plant_position():
+	var plant = find_nearest_plant()
+	if plant == null:
 		return null
-	return closest_plant.global_position
-
+	return plant.global_position
+	
+func find_plant_in_range():
+	var plants = get_tree().get_nodes_in_group("Plants")
+	for plant:CornPlant in plants:
+		if global_position.distance_to(plant.global_position) <= BITE_RANGE:
+			return plant
+	return null
 
 
 func _on_recalibration_timer_timeout() -> void:
+	recalibrate()
+	
+func recalibrate() -> void:
 	var new_target = find_nearest_plant_position()
 	if new_target == target: return
 	target = null
 	hesitation.start()
 	await hesitation.timeout
 	target = new_target
+	
+func try_to_bite():
+	target = null
+	bite_timer.start()
+	while true:
+		await bite_timer.timeout
+		var plant: CornPlant = find_plant_in_range()
+		if plant == null:
+			bite_timer.stop()
+			recalibrate()
+			return
+		if plant.num_corns <= 0:
+			eaten_plants_locations[plant.global_position] = true
+			bite_timer.stop()
+			recalibrate()
+			return
+		plant.bite()
+	
 	
